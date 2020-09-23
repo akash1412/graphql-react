d23878c6a66cda7eb6a145c0af2f0977969186a6
akash prasad <akashorasad2000@gmail.com>
2020-09-24 03:30:55 +0530

    added dynamic relation into the app

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
-const express=require('express');
-const {graphqlHTTP}=require('express-graphql');
-const {buildSchema}=require('graphql')
-const mongoose=require('mongoose')
-const Event=require('./models/events');
-const { findById } = require('./models/users');
-const User=require('./models/users')
+const express = require('express');
+const {
+  graphqlHTTP
+} = require('express-graphql');
+const {
+  buildSchema
+} = require('graphql')
+const mongoose = require('mongoose')
+const Event = require('./models/events');
 
-const app=express();
+const User = require('./models/users')
+
+const app = express();
 
 app.use(express.json())
 
- 
+const events = async eventIds => {
 
-app.use('/graphql',graphqlHTTP({
-    schema:buildSchema(`
+
+  const events = await Event.find({
+    _id: {
+      $in: eventIds
+    }
+  })
+
+  return events.map((event) => {
+    return {
+      ...event._doc,
+      date: new Date(event._doc.date).toISOString(),
+      creator: user.bind(this, event._doc.creator)
+    }
+  })
+
+}
+
+const user = async (userId) => {
+
+
+  try {
+    const user = await User.findById(userId);
+
+
+    return {
+      ...user._doc,
+      createdEvents: events.bind(this, user._doc.createdEvents)
+    }
+
+  } catch (error) {
+    throw error
+  }
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
     
     type Event {
       _id: ID!
@@ -21,16 +59,19 @@ app.use('/graphql',graphqlHTTP({
       description: String!
       price: Float!
       date: String!
+      creator:User!
     }
 
     type User {
       _id: ID!
       email:String!
       password:String
+      createdEvents:[Event!]
     }
 
      type rootQuery {
        events:[Event!]!
+       user(userId:String!) : User!
      }
 
       input EventInput {
@@ -56,74 +97,112 @@ app.use('/graphql',graphqlHTTP({
           mutation:rootMutation
       }
     `),
-    rootValue:{
-        events:async()=>{
-          const events= await Event.find();
+  rootValue: {
+    events: async () => {
+      try {
 
-          return events
-        },
-        createEvent:async (args)=>{
- 
-          try {
-            const event =  await Event.create({
-              title:args.eventInput.title,
-                 description:args.eventInput.description,
-                 price:+args.eventInput.price,
-                 date:new Date( args.eventInput.date),
-                 creator:"5f69fa6ea3ab77fcc578f589"
-            })
-             
-            const user=await User.findById("5f69fa6ea3ab77fcc578f589")
+        const events = await Event.find()
 
-            user.createdEvents=[...user.createdEvents,event];
-
-            user.save()
-
-            return event
-          } catch (error) {
-            console.log(error);
-            throw error
+        return events.map((event) => {
+          return {
+            ...event._doc,
+            date: new Date(event._doc.date).toISOString(),
+            creator: user.bind(this, event._doc.creator)
           }
-      
-        },
-        deleteEvent:async()=>{
-          await Event.deleteMany()
-          return 'Events Deleted'
-        },
-        createUser:async(args)=>{
-       
-          const {email,password}=args.userInput;
-          
-          const UserExists= await User.findOne({email})
-           
-          if(UserExists){
-            throw new Error('User alredy exist')
-          }
+        })
 
-          const newUser= await User.create({
-             email,password
-           })
-           
-           newUser.password=null
 
-           return newUser
+      } catch (error) {
+        throw error
+
+      }
+    },
+    createEvent: async (args) => {
+
+      try {
+        const newEvent = await Event.create({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5f6bc39588d817f74a10ee8b"
+        })
+
+        const event = {
+          ...newEvent._doc,
+          date: new Date(newEvent._doc.date).toISOString(),
+          creator: user.bind(this, newEvent._doc.creator)
         }
+        const dbUser = await User.findById("5f6bc39588d817f74a10ee8b")
+
+        dbUser.createdEvents = [...dbUser.createdEvents, newEvent];
+
+        dbUser.save()
+
+        return event
+      } catch (error) {
+        console.log(error);
+        throw error
+      }
 
     },
-    graphiql:true
+    deleteEvent: async () => {
+      try {
+        await Event.deleteMany()
+        return 'Events Deleted'
+      } catch (error) {
+        console.error(error)
+      }
+
+    },
+
+    createUser: async (args) => {
+
+      const {
+        email,
+        password
+      } = args.userInput;
+
+      try {
+
+        const UserExists = await User.findOne({
+          email
+        })
+
+        if (UserExists) {
+          throw new Error('User alredy exist')
+        }
+
+        const newUser = await User.create({
+          email,
+          password
+        })
+
+        console.log(newUser)
+        newUser.password = null
+
+        return newUser
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
+
+  },
+  graphiql: true
 }))
 
-const DB=process.env.DB_URL.replace('<PASSWORD>',process.env.DB_PASSWORD)
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD)
 
- 
-  mongoose.connect(DB,{ 
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-    }).then(()=>console.log('connected to DB')).catch((error)=>console.log(error))
 
-      
- 
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('connected to DB')).catch((error) => console.log(error))
 
-app.listen(4000,()=>{
-    console.log('graphql server running ')
+
+
+
+app.listen(4000, () => {
+  console.log('graphql server running ')
 })
--- models/users.js
+++ models/users.js
@@ -1,34 +1,31 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema
 
-const userSchema=new Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:[true,'User with this email already exist\'s ']
-    }
-    ,
-    password:{
-        type:String,
-        required:true
-    }
-    ,
-    createdEvents:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:'Events'
-        }
-    ]
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: [true, 'User with this email already exist\'s ']
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdEvents: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Events'
+    }]
 });
 
-userSchema.pre('save',async function(next){
- this.password =await bcrypt.hash(this.password,12);
 
- next()
+userSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 12);
+
+    next()
 })
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports=User;
+module.exports = User;
